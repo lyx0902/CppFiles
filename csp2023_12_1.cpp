252e61a75e26c788d204dff584daffb5dab971de
lyx0902 <320979308@qq.com>
2024-09-18 10:04:54 +0800

    Changes

--- csp2023_12_1.cpp
+++ csp2023_12_1.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <vector>
+#include<algorithm>
 using namespace std;
 int main(){
     int n,m;
@@ -13,17 +13,24 @@ int main(){
     for(int i=0;i<n;i++){
         int flag=0;//当前仓库编号
         int temp[11]={0};
+        int max1=1020;
         for(int p=0;p<m;p++){
             temp[p]=a[i][p];
         }
         for(int j=0;j<n;j++){
-            for(int k=0;k<m;k++){
-                if(a[j][k]<temp[k]){
-                    flag=j;
+            if(flag!=0){
+                break;
+            }
+            for(int k=0;k<m;k++) {
+                if (a[j][k] > temp[k]) {
+                    if (k == m - 1) {
+                        flag = j + 1;
+                    }
                     continue;
                 }
                 break;
             }
+            flag=min(max1,flag);
         }
         cout<<flag<<endl;
     }
