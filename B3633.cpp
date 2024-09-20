6f391fa4dafd61ad9c8fcf7e169a3f21d46fb03e
lyx0902 <320979308@qq.com>
2024-09-20 16:01:43 +0800

    Changes

---
+++ B3633.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <set>
+using namespace std;
+set<int>s1;
+set<int>s2;
+set<int>combined;
+set<int>result;
+int main(){
+    int x,y;
+    cin>>x;
+    if(x!=0){
+
+    }
+}
